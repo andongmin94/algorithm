b3ca9d455f84ae1a158f4d49ab6643b8c064f4e4
안동민 <andongmin94@gmail.com>
2023-12-25 19:13:12 +0900

    [Silver II] Title: 에디터, Time: 72 ms, Memory: 21020 KB -BaekjoonHub

---
+++ 백준/Silver/1406. 에디터/에디터.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    string a;
+    cin >> a;
+
+    list<char> b;
+    for (char e : a)
+        b.push_back(e);
+    list<char>::iterator t = b.end();
+
+    int k;
+    cin >> k;
+    
+    for (int i = 0; i < k; i++)
+    {
+        char command;
+        cin >> command;
+        if (command == 'P')
+        {
+            char command2;
+            cin >> command2;
+            b.insert(t,command2);
+        }
+        if (command == 'L' && t != b.begin())
+            --t;
+        if (command == 'D' && t != b.end())
+            ++t;
+        if (command == 'B' && t != b.begin())
+        {
+            if (t == b.end())
+            {
+                --t;
+                t = b.erase(t);
+            }
+            else
+            {
+                --t;
+                t = b.erase(t);
+            }
+        }
+    }
+    for (auto e : b)
+        cout << e;
+}
