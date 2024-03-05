c3b8f150b5d842b3d749bd1b5424c46637d1dd4e
안동민 <andongmin94@gmail.com>
2024-03-05 17:45:53 +0900

    [level 0] Title: 문자열 반복해서 출력하기, Time: 203.69 ms, Memory: 67.1 MB -BaekjoonHub

--- 프로그래머스/0/181950. 문자열 반복해서 출력하기/README.md
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열 반복해서 출력하기 - 181950 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181950?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181950) 
 
 ### 성능 요약
 
-메모리: 62.7 MB, 시간: 138.77 ms
+메모리: 67.1 MB, 시간: 203.69 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 02일 00:54:21
+2024년 03월 05일 17:45:51
 
 ### 문제 설명
 
--- 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
+++ 프로그래머스/0/181950. 문자열 반복해서 출력하기/문자열 반복해서 출력하기.java
@@ -1,13 +1,14 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class Solution {
+class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
         int n = sc.nextInt();
 
-        for (int i = 0; i < n; i++)
+        while(n > 0) {
             System.out.print(str);
-
+            n--;
+        }
     }
 }
