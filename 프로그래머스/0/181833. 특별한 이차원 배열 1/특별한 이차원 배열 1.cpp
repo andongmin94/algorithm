df8f29fa545c9f10f51ce203b4fb2dbd55260976
안동민 <andongmin94@gmail.com>
2024-08-09 03:50:02 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 2.14 ms, Memory: 5.87 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.cpp
@@ -8,10 +8,8 @@ vector<vector<int>> solution(int n) {
     //
     vector<vector<int>> arr(n, vector<int>(n, 0));
     for (int i = 0; i < n; i++)
-    {
         for (int j = 0; j < n; j++)
             if (i == j) arr[i][j] = 1;
-    }
     answer = arr;
     //
     return answer;
