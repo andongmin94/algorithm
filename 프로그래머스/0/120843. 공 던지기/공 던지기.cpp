7e6b9b74d5cfc7c1c6077ba36feb56352e837603
안동민 <andongmin94@gmail.com>
2024-08-18 00:07:15 +0900

    [level 0] Title: 공 던지기, Time: 0.04 ms, Memory: 4.21 MB -BaekjoonHub

--- 프로그래머스/0/120843. 공 던지기/공 던지기.cpp
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.cpp
@@ -5,16 +5,5 @@ using namespace std;
 
 int solution(vector<int> numbers, int k) {
     int answer = 0;
-    //
-    while(--k)
-    {
-        if (answer == numbers.size() + 1) answer = 1;
-        answer++;
-        if (answer == numbers.size() + 1) answer = 1;
-        answer++;
-        if (answer == numbers.size() + 1) answer = 1;
-    }
-    answer++;
-    //
     return answer;
 }
