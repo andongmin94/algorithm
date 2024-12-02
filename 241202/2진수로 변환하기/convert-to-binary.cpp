d256dc3e869f005acb0038b4774909c0b1a81adf
안동민 <andongmin94@gmail.com>
2024-12-02 16:54:58 +0900

    Title: 2진수로 변환하기, Time: 5ms, Memory: 0MB, Status: Passed - CodeTree

--- 241202/2진수로 변환하기/convert-to-binary.cpp
+++ 241202/2진수로 변환하기/convert-to-binary.cpp
@@ -3,20 +3,19 @@ using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n = 29;
-    int digits[20] = {};
+    int n;
+    cin >> n;
     int cnt = 0;
+    int digit[100] = {};
     while(true)
     {
-        if (n < 2)
-        {
-            digits[cnt] = n;
-            break;
-        }
-        digits[cnt++] = n % 2;
+        if (n < 2) digit[cnt] = n;
+        if (n < 2) break;
+
+        digit[cnt++] = n % 2;
         n /= 2;
     }
     for (int i = cnt; i >= 0; i--)
-        cout << digits[i];
+        cout << digit[i];
     return 0;
 }
