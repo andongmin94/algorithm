ed1cec6f74ee60b1ff9b97b1b9206812d7e29c7f
안동민 <andongmin94@gmail.com>
2025-01-13 13:38:43 +0900

    Title: 계속 중첩되는 사각형, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 250113/계속 중첩되는 사각형/continuously-overlapping-squares.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int x1, y1;
+int x2, y2;
+int arr[300][300];
+int cnt = 0;
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> x1 >> y1 >> x2 >> y2;
+        x1 += 100;
+        x2 += 100;
+        y1 += 100;
+        y2 += 100;
+        if (i % 2)
+        {
+            for (int j = y1; j < y2; j++)
+            {
+                for (int k = x1; k < x2; k++)
+                {
+                    if (!arr[j][k])
+                    {
+                        arr[j][k]++;
+                        cnt++;
+                    }
+                }
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
