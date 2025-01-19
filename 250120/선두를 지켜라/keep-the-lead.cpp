443bacec180065c555d4374c4960d96932ffe92a
안동민 <andongmin94@gmail.com>
2025-01-20 01:04:04 +0900

    Title: 선두를 지켜라, Time: 10ms, Memory: 2MB, Status: Passed - CodeTree

--- 250120/선두를 지켜라/keep-the-lead.cpp
+++ 250120/선두를 지켜라/keep-the-lead.cpp
@@ -1,41 +1,45 @@
 #include <iostream>
 
+#define MAX_T 1000000
+
 using namespace std;
 
-int N, M;
-int v[1000], t[1000];
-int v2[1000], t2[1000];
-int a_arr[1000000 + 1];
-int b_arr[1000000 + 1];
+int n, m;
+int pos_a[MAX_T + 1], pos_b[MAX_T + 1];
 
 int main() {
-    cin >> N >> M;
-
-    int a = 1;
-    for (int i = 0; i < N; i++)
-    {
-        cin >> v[i] >> t[i];
-        for (int j = 0; j < t[i]; j++)
-        {
-            a_arr[a++] += v[i]; 
+    // 입력
+    cin >> n >> m;
+    
+    // A가 매 초마다 서있는 위치를 기록
+    int time_a = 1;
+    for(int i = 0; i < n; i++) {
+        int v, t;
+        cin >> v >> t;
+        
+        while(t--) {
+            pos_a[time_a] = pos_a[time_a - 1] + v;
+            time_a++;
         }
     }
-
-    int b = 1;
-    for (int i = 0; i < M; i++)
-    {
-        cin >> v2[i] >> t2[i];
-        for (int j = 0; j < t2[i]; j++)
-        {
-            b_arr[b++] += v2[i]; 
+    
+    // B가 매 초마다 서있는 위치를 기록
+    int time_b = 1;
+    for(int i = 0; i < m; i++) {
+        int v, t;
+        cin >> v >> t;
+        
+        while(t--) {
+            pos_b[time_b] = pos_b[time_b - 1] + v;
+            time_b++;
         }
     }
-
+    
+    // A와 B 중 더 앞서 있는 경우를 확인합니다.
+    // A가 리더면 1, B가 리더면 2로 관리합니다.
     int leader = 0, ans = 0;
-    for(int i = 1; i < a; i++)
-    {
-        if(a_arr[i] > b_arr[i])
-        {
+    for(int i = 1; i < time_a; i++) {
+        if(pos_a[i] > pos_b[i]) {
             // 기존 리더가 B였다면
             // 답을 갱신합니다.
             if(leader == 2)
@@ -44,8 +48,7 @@ int main() {
             // 리더를 A로 변경합니다.
             leader = 1; 
         }
-        else if(a_arr[i] < b_arr[i])
-        {
+        else if(pos_a[i] < pos_b[i]) {
             // 기존 리더가 A였다면
             // 답을 갱신합니다.
             if(leader == 1)
