a88be866fe1291f2a7f2fd71b3f2df684d7fca8c
안동민 <andongmin94@gmail.com>
2025-04-12 18:48:59 +0900

    Title: 정수의 조건 여부 3, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250412/정수의 조건 여부 3/numbers-condition-3.cpp
+++ 250412/정수의 조건 여부 3/numbers-condition-3.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main() {
     // Please write your code here.
     int n; cin >> n;
-    if (a % 13 == 0 || a % 19 == 0) cout << "True";
+    if (n % 13 == 0 || n % 19 == 0) cout << "True";
     else cout << "False";
     return 0;
 }
