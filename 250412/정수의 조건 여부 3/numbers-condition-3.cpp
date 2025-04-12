4a65e365eb13774b5196bb62e8932cafbc534fcf
안동민 <andongmin94@gmail.com>
2025-04-12 18:48:32 +0900

    Title: 정수의 조건 여부 3, Time: -1ms, Memory: -1MB, Status: Compile Error - Codetree

---
+++ 250412/정수의 조건 여부 3/numbers-condition-3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    int n; cin >> n;
+    if (a % 13 == 0 || a % 19 == 0) cout << "True";
+    else cout << "False";
+    return 0;
+}
