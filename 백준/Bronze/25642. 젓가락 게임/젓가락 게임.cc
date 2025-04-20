724efbdf6139c1da2d4aef17615ee98a1f2aaed9
안동민 <andongmin94@gmail.com>
2025-04-21 08:22:24 +0900

    [Bronze III] Title: 젓가락 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25642. 젓가락 게임/젓가락 게임.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a, b; cin >> a >> b;
+    bool turn = true;
+    while (a < 5 && b < 5)
+    {
+        if (turn)
+        {
+            b += a;
+            turn = !turn;
+        }
+        else
+        {
+            a += b;
+            turn = !turn;
+        }
+    }
+    if (a < b) cout << "yt";
+    else cout << "yj";
+}
