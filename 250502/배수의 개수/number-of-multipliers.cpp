aab1acf3366adc81a48b72afc069927101d70ba7
안동민 <andongmin94@gmail.com>
2025-05-02 16:58:19 +0900

    Title: 배수의 개수, Time: 6ms, Memory: 0MB, Status: Passed - Codetree

--- 250502/배수의 개수/number-of-multipliers.cpp
+++ 250502/배수의 개수/number-of-multipliers.cpp
@@ -3,12 +3,13 @@ using namespace std;
 
 int main() {
     // Please write your code here.
-    int n = 10, cnt = 0;
+    int n = 10, cnt = 0, cnt2 = 0;
     while(n--)
     {
         int a; cin >> a;
-        if (a % 3 == 0 || a % 5 == 0) cnt++;
+        if (a % 3 == 0) cnt++;
+        if (a % 5 == 0) cnt2++;
     }
-    cout << cnt;
+    cout << cnt << ' ' << cnt2;
     return 0;
 }
