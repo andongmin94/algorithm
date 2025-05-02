05ad7baeb858ca98136e4aba11caff3da9055ca7
안동민 <andongmin94@gmail.com>
2025-05-02 13:29:53 +0900

    Title: 친근하지 않은 수, Time: 5ms, Memory: 0MB, Status: Passed - Codetree

--- 250502/친근하지 않은 수/unfriendly-number.cpp
+++ 250502/친근하지 않은 수/unfriendly-number.cpp
@@ -5,7 +5,7 @@ int main() {
     // Please write your code here.
     int n, cnt = 0; cin >> n;
     for (int i = 1; i <= n; i++)
-        if (i % 2 || i % 3 || i % 5) cnt++;
+        if (i % 2 && i % 3 && i % 5) cnt++;
     cout << cnt;
     return 0;
 }
