d6edfe3b382c8c82e0a0fedeb80ec13d8a4bae7a
안동민 <andongmin94@gmail.com>
2025-05-07 16:02:59 +0900

    Title: 숫자 피라미드, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 250507/숫자 피라미드/number-pyramid.cpp
+++ 250507/숫자 피라미드/number-pyramid.cpp
@@ -7,7 +7,7 @@ int main() {
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < i + 1; j++)
-            cout << i + 1;
+            cout << i + 1 << ' ';
         cout << '\n';
     }
     return 0;
